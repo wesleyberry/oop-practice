c71cca5f819eac95795fd0f89e0212f9cc69b0e3
Wesley Berry <wesleyberry52@gmail.com>
2019-12-27 11:26:39 -0600

    created class about professor and room nums

--- index.js
+++ index.js
@@ -23,76 +23,93 @@
 
 // --------------------------------------------------
 
-class User {
-    constructor(username, email, password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
+// class User {
+//     constructor(username, email, password) {
+//         this.username = username;
+//         this.email = email;
+//         this.password = password;
+//     }
 
-    static countUsers() {
-        console.log(`There are 50 users.`);
-    }
+//     static countUsers() {
+//         console.log(`There are 50 users.`);
+//     }
 
-    register() {
-        console.log(`${this.username} is now registered.`);
-    }
-}
+//     register() {
+//         console.log(`${this.username} is now registered.`);
+//     }
+// }
 
-let bob = new User('Bob1', 'bob@bob.com', 'bob123');
-bob.register();
+// let bob = new User('Bob1', 'bob@bob.com', 'bob123');
+// bob.register();
 
-class Member extends User {
-    constructor(username, email, password, memberPackage) {
-        super(username, email, password);
-        this.package = memberPackage;
-    }
+// class Member extends User {
+//     constructor(username, email, password, memberPackage) {
+//         super(username, email, password);
+//         this.package = memberPackage;
+//     }
 
-    getPackage() {
-        console.log(`${this.username} is subscribed to the ${this.package} package.`)
-    }
-}
+//     getPackage() {
+//         console.log(`${this.username} is subscribed to the ${this.package} package.`)
+//     }
+// }
 
-let mike = new Member('Mike', 'mike@mike.com', 'password', 'Standard');
+// let mike = new Member('Mike', 'mike@mike.com', 'password', 'Standard');
 // mike.getPackage();
-mike.register();
+// mike.register();
 
-class className {
-    constructor(type, roomNum, professor) {
-        this.type = type;
+// class className {
+//     constructor(type, roomNum, professor) {
+//         this.type = type;
+//         this.roomNum = roomNum;
+//         this.professor = professor;
+//     }
+
+//     tellRoomNum() {
+//         console.log(`This is room #${this.roomNum}`);
+//     }
+
+//     changeRoomNum(newNum) {
+//         this.roomNum = newNum;
+//         console.log(`The new room number is #${this.roomNum}`);
+//     }
+// }
+
+// let class1 = new className('English', 224, 'Wazowski');
+// class1.tellRoomNum();
+// class1.changeRoomNum(240);
+// class1.tellRoomNum();
+
+// ------------------------------------
+
+// function Player(name, favGame) {
+//     this.name = name,
+//     this.favGame = favGame,
+//     this.play = function() {
+//         console.log(`${this.name} is playing ${this.favGame}`);
+//     }
+//     this.newFavGame = function(newGame) {
+//         this.favGame = newGame;
+//         console.log(`${this.name}'s new favorite game is ${this.favGame}`);
+//     }
+// }
+
+// let player1 = new Player('John', 'Call of Duty');
+// player1.play();
+// player1.newFavGame('Mario Cart');
+
+// -------------------------------------------
+
+class classDetails {
+    constructor(study, roomNum, professor) {
+        this.study = study;
         this.roomNum = roomNum;
         this.professor = professor;
     }
 
-    tellRoomNum() {
-        console.log(`This is room #${this.roomNum}`);
-    }
-
-    changeRoomNum(newNum) {
-        this.roomNum = newNum;
-        console.log(`The new room number is #${this.roomNum}`);
+    getClassName() {
+        console.log(`Dr. ${this.professor} teaches ${this.study} in room #${this.roomNum}`);
     }
 }
 
-let class1 = new className('English', 224, 'Wazowski');
-class1.tellRoomNum();
-class1.changeRoomNum(240);
-class1.tellRoomNum();
-
-// ------------------------------------
-
-function Player(name, favGame) {
-    this.name = name,
-    this.favGame = favGame,
-    this.play = function() {
-        console.log(`${this.name} is playing ${this.favGame}`);
-    }
-    this.newFavGame = function(newGame) {
-        this.favGame = newGame;
-        console.log(`${this.name}'s new favorite game is ${this.favGame}`);
-    }
-}
-
-let player1 = new Player('John', 'Call of Duty');
-player1.play();
-player1.newFavGame('Mario Cart');
+let class1 = new classDetails('English', 400, 'Zelinski');
+class1.getClassName();
